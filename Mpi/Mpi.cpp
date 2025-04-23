e77a34f4a143d1502b61b4937d65312424306f21
Rokiya Abd Elsatar <rokiyaabdelstar293@gmail.com>
2025-04-24 01:03:36 +0200

    remove unused array

--- Mpi/Mpi.cpp
+++ Mpi/Mpi.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	MPI_Init(NULL,NULL);
-	int rank, size, processID[8];
+	int rank, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	printf("Hello from process %d out of %d processes\n", rank, size);
