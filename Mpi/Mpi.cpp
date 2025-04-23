ecb91051a5eeabfe677c7de3a5c739284f5da06c
Rokiya Abd Elsatar <rokiyaabdelstar293@gmail.com>
2025-04-24 01:02:13 +0200

    first task using MPI

---
+++ Mpi/Mpi.cpp
@@ -0,0 +1,17 @@
+// Mpi.cpp : This file is a simple task to print hello using all process
+//
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main()
+{
+	MPI_Init(NULL,NULL);
+	int rank, size, processID[8];
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("Hello from process %d out of %d processes\n", rank, size);
+
+
+	MPI_Finalize();
+}
